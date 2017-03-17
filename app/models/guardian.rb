5a18aa5b8d60eb8167f75f8a753f586951850069
JD Hiudt <email": null>
2017-03-17 17:07:33 -0500

    populate guardian table and model

--- app/models/guardian.rb
+++ app/models/guardian.rb
@@ -1,2 +1,7 @@
 class Guardian < ApplicationRecord
+  has_many :memories
+  belongs_to :user
+
+  validates :user_id, :title, :image, presence: true
+
 end
--- db/migrate/20170317220121_create_guardians.rb
+++ db/migrate/20170317220121_create_guardians.rb
@@ -1,8 +1,10 @@
 class CreateGuardians < ActiveRecord::Migration[5.0]
   def change
     create_table :guardians do |t|
-
-      t.timestamps
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.string :image, null: false
+      t.timestamps(null: false)
     end
   end
 end
